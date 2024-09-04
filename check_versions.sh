a5cdb07137c2818155ea86412e5ce2561cb63804
Mohammed-Farhaan-N <mohammedfarhaan235@gmail.com>
2024-09-04 21:39:46 +0530

    update check version

--- check_versions.sh
+++ check_versions.sh
@@ -1,56 +1,58 @@
 #!/bin/bash
 
-echo "Checking versions of installed tools..."
-
+echo "=============================================="
+echo "     Checking Versions of Installed Tools     "
+echo "=============================================="
 
 # Java
-echo -n "Java: "
+echo -e "\nJava: "
 java -version 2>&1 | head -n 1 || echo "Not installed"
 
 # AWS CLI
-echo -n "AWS CLI: "
+echo -e "\nAWS CLI: "
 aws --version 2>/dev/null || echo "Not installed"
 
 # Terraform
-echo -n "Terraform: "
+echo -e "\nTerraform: "
 terraform version 2>/dev/null || echo "Not installed"
 
 # Ansible
-echo -n "Ansible: "
+echo -e "\nAnsible: "
 ansible --version 2>/dev/null | head -n 1 || echo "Not installed"
 
 
 # Docker
-echo -n "Docker: "
+echo -e "\nDocker: "
 docker --version 2>/dev/null || echo "Not installed"
 
 # Trivy
-echo -n "Trivy: "
+echo -e "\nTrivy: "
 trivy --version 2>/dev/null || echo "Not installed"
 
-# SonarQube 
-echo "Checking for running SonarQube container running port 9000..."
+# SonarQube
+echo -e "\nChecking for running SonarQube container running port 9000..."
+echo -e "\n"
 docker ps --filter "name=sonarqube"
 
-# Nexus 
-echo "Checking for running nexus container running port 8081..."
+# Nexus
+echo -e "\nChecking for running nexus container running port 8081..."
+echo -e "\n"
 docker ps --filter "name=nexus"
 
 # kubectl
-echo -n "kubectl: "
-kubectl version --client --short 2>/dev/null || echo "Not installed"
+echo -e "\nkubectl: "
+kubectl version --client  2>/dev/null || echo "Not installed"
 
 # Helm
-echo -n "Helm: "
-helm version --short 2>/dev/null || echo "Not installed"
+echo -e "\nHelm: "
+helm version  --short 2>/dev/null || echo "Not installed"
 
 # eksctl
-echo -n "eksctl: "
+echo -e "\neksctl: "
 eksctl version 2>/dev/null || echo "Not installed"
 
 
+echo -e "\n=============================================="
+echo "           Version Check Completed            "
+echo "=============================================="
 
-
-
-
-echo "Version check completed."
