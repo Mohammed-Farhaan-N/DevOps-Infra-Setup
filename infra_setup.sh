20786bee0620e5c73807d01067e282e82f99ab80
Mohammed Farhaan <85581399+Mohammed-Farhaan-N@users.noreply.github.com>
2024-09-07 01:01:09 +0530

    Update infra_setup.sh

--- infra_setup.sh
+++ infra_setup.sh
@@ -1,8 +1,9 @@
-chmod +x /Images/Default.sh Jenkins.sh /Kubenetes/kube-config.sh /CI-CD/Artifactory.sh /CI-CD/Testing_Environment.sh 
-chmod +x /CI-CD/Bypass_script/bypass_jenkins_initial_pass.sh /CI-CD/Bypass_script/bypass_nexus_initial_pass_docker.sh /CI-CD/bypassed_Artifactory.sh
-chmod +x Kube_setup.sh full-CI-CD-setup.sh check_versions.sh
+chmod +x Images/*.sh
+chmod +x Kubenetes/*.sh
+chmod +x CI-CD/*.sh
+chmod +x CI-CD/Bypass_script/*.sh
+chmod +x *.sh
 
 sudo bash ./Images/Default.sh
-
 
 #created by https://github.com/Mohammed-Farhaan-N
